e4a97499d52f5dbd0b37474eacf392c067ca1b2b
NekrodNIK <60639354+NekrodNIK@users.noreply.github.com>
2021-10-06 19:37:49 +0500

    add style myMessage

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from os import path
 
 import sass
 
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QSizePolicy
+
 from interfaces.main_window import Ui_MainWindow
+from modules.MessageElement import MessageElement
 
 
 class MainWindow(Ui_MainWindow, QMainWindow):
@@ -29,10 +31,25 @@ class MainWindow(Ui_MainWindow, QMainWindow):
         print(text_css)
 
         self.setStyleSheet(text_css)
+
+        self.add_message("my", "1111")
     
-    # TODO Сделать добавление сообщений в чат
-    # def add_message(self, text: str, time):
-        # self.MessageAreaContentLayout.addChildWidget()
+    def add_message(self, type: str, text: str):
+        new_message = MessageElement()
+        new_message.setText(text)
+        new_message.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Minimum)
+        
+        if type == "my":
+            new_message.setObjectName("myMessage")
+
+            self.MessageAreaContentLayout.addWidget(QLabel())
+            self.MessageAreaContentLayout.addWidget(new_message)
+
+        elif type == "other_user":
+            new_message.setObjectName("otherUserMessage")
+
+            self.MessageAreaContentLayout.addWidget(new_message)
+            self.MessageAreaContentLayout.addWidget(QLabel())
 
 
 if __name__ == "__main__":

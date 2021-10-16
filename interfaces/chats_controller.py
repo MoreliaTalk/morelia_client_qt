1a1695de544ec40a269c62624e5231e563e7a1db
NekrodNIK <60639354+NekrodNIK@users.noreply.github.com>
2021-10-16 13:36:30 +0500

    fix image radius

--- interfaces/chats_controller.py
+++ interfaces/chats_controller.py
@@ -29,7 +29,7 @@ class ChatItem(Ui_ContactCard, QtWidgets.QWidget):
                 ]
             )
 
-            big_size = (image_result.size[0]*3, image_result.size[0]*3)
+            big_size = (image_result.size[0]*4, image_result.size[0]*4)
             mask = Image.new("L", big_size)
             draw = ImageDraw.Draw(mask)
             draw.ellipse((0, 0) + big_size, fill=255)
@@ -62,45 +62,6 @@ class ChatsController:
     def add_chat(self, chatName: str, lastMessageText: str, image: Image.Image = None):
         new_chat_item = ChatItem(chatName, lastMessageText, image)
         self.ChatsContentLayout.addWidget(new_chat_item)
-
-"""        
-        new_contact_card = QtWidgets.QWidget()
-        ui = Ui_ContactCard()
-        ui.setupUi(new_contact_card)
-
-        ui.ChatNameLabel.setText(contactName)
-        ui.ChatLastMessageLabel.setText(lastMessageText)
-        if imgPath:
-            labelSize = ui.ContactAvatar.width()
-            sourcePixmap = QPixmap(path.join("interfaces", "ui", imgPath))
-            imgSize = min(sourcePixmap.width(), sourcePixmap.height())
-            croppedPixmap = sourcePixmap.copy(int((sourcePixmap.width()-imgSize)/2),
-                                              int((sourcePixmap.height()-imgSize)/2),
-                                              imgSize, imgSize)
-            smallerPixmap = croppedPixmap.scaled(labelSize, labelSize, transformMode=Qt.SmoothTransformation)
-            ui.ContactAvatar.target = QPixmap(labelSize, labelSize)
-            ui.ContactAvatar.target.fill(Qt.transparent)
-            painter = QPainter(ui.ContactAvatar.target)
-            painter.setRenderHint(QPainter.Antialiasing, True)
-            painter.setRenderHint(QPainter.HighQualityAntialiasing, True)
-            painter.setRenderHint(QPainter.SmoothPixmapTransform, True)
-            painterPath = QPainterPath()
-            painterPath.addRoundedRect(
-                0, 0, labelSize, labelSize,
-                labelSize/2, labelSize/2)
-            painter.setClipPath(painterPath)
-            painter.drawPixmap(0, 0, smallerPixmap)
-            ui.ContactAvatar.setPixmap(ui.ContactAvatar.target)
-        else:
-            namesWord = contactName.split(" ")
-            ui.ContactAvatar.setText(namesWord[0][0] + namesWord[1][0])
-            ui.ContactAvatar.setStyleSheet(
-                f"background-color: rgb({randrange(100)},{randrange(100)},{randrange(100)});")
-
-        self.ChatsContentLayout.addWidget(new_contact_card)
-        return new_contact_card
-"""
-
 
 if __name__ == "__main__":
     import sys

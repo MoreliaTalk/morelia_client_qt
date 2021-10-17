f7108418c3a2e9bdf4aeb42b0c5766d1b71a956e
NekrodNIK <60639354+NekrodNIK@users.noreply.github.com>
2021-10-17 16:28:45 +0500

    add logging with loguru

---
+++ modules/logging.py
@@ -0,0 +1,31 @@
+import sys
+from loguru import logger
+
+
+def set_logger_setting():
+    logger.remove()
+
+    logger.add(
+        sys.stdout,
+        format=(
+            "<lvl>{level}</lvl>" +
+            " | <green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | " +
+            "<cyan>{message}</cyan>"
+        ),
+        level="INFO",
+        colorize=True,
+        enqueue=True
+    )
+
+    logger.add(
+        "./log/errors.log",
+        format=(
+            "{level}" +
+            " | {time:YYYY-MM-DD HH:mm:ss.SSS} | " +
+            "{file} (function: {function}, line: {line}) | " +
+            "{message}"
+        ),
+        level="ERROR",
+        colorize=False,
+        enqueue=True
+    )

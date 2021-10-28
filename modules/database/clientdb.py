283529c11fd1aeec34d8e621b7a7374d661ae18e
rus-ai <russian-ai@yandex.ru>
2021-10-28 20:48:49 +0300

    NamedTuple naming refactor

--- modules/database/clientdb.py
+++ modules/database/clientdb.py
@@ -108,27 +108,30 @@ class ClientDb:
     @staticmethod
     def list_flow():
         list_flow = []
-        Flow = namedtuple('Flow', 'id uuid title last_message last_time count')
+        FlowTuple = namedtuple('FlowTuple',
+                               'id uuid title last_message last_time count')
         db_query = models.Flow.selectBy()
         for flow in db_query:
             second_query = models.Message.selectBy(flow=flow.id).orderBy("-time")
             if second_query.count():
-                list_flow.append(Flow(flow.id, flow.uuid, flow.title,
+                list_flow.append(FlowTuple(flow.id, flow.uuid, flow.title,
                                       second_query[0].text, second_query[0].time,
                                       second_query.count()))
             else:
-                list_flow.append(Flow(flow.id, flow.uuid, flow.title,
+                list_flow.append(FlowTuple(flow.id, flow.uuid, flow.title,
                                       "", 0, 0))
         for line in sorted(list_flow, key=attrgetter('last_time'), reverse=True):
             yield line
 
     @staticmethod
     def list_messages(flow_id):
-        Message = namedtuple('Message', 'id uuid text time user_id user_uuid username')
+        MessageTuple = namedtuple('MessageTuple',
+                                  'id uuid text time user_id user_uuid username')
         db_query = models.Message.selectBy(flow=flow_id).orderBy("-time")
         for line in db_query:
             user = models.UserConfig.selectBy(id=line.user).getOne()
-            yield Message(line.id, line.uuid, line.text, line.time, user.id, user.uuid, user.username)
+            yield MessageTuple(line.id, line.uuid, line.text, line.time,
+                               user.id, user.uuid, user.username)
 
     @staticmethod
     def get_user_id_by_uuid(user_uuid):

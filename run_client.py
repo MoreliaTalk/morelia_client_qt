67fd5de7108738f3902a814c3bdab1d59519f207
rus-ai <russian-ai@yandex.ru>
2021-12-25 17:02:25 +0300

    Create Login Dialog UI and Styles

--- run_client.py
+++ run_client.py
@@ -2,9 +2,12 @@ from PySide6.QtWidgets import QApplication
 import sys
 
 from main import MainWindow
+from interfaces.login import LoginDialog
 
 
 app = QApplication(sys.argv)
+login = LoginDialog(app)
+login.exec()
 window = MainWindow(app)
 window.show()
 sys.exit(app.exec())

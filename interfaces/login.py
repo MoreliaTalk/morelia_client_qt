34472b708453e7c774ae3ba4c1f2e521520115c0
rus-ai <russian-ai@yandex.ru>
2021-12-26 03:41:45 +0300

    Functions annotation and docstrings

--- interfaces/login.py
+++ interfaces/login.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 from PySide6.QtGui import QShortcut, QKeySequence
 
-from PySide6.QtWidgets import QApplication, QDialog
+from PySide6.QtWidgets import QApplication, QDialog, QLineEdit, QWidget
 
 from interfaces.raw.login_dialog import Ui_loginDialog
 from interfaces.helpers import load_font, set_color_theme
@@ -13,7 +13,15 @@ from modules.logging import set_logger_setting
 set_logger_setting()
 
 
-def is_filled(line_edit):
+def is_filled(line_edit: QLineEdit) -> bool:
+    """Check if text property filled,
+    if not add dynamic property "wrongInput" for redraw style,
+    and invoke redraw_widget() for apply style changes
+    args:
+        line_edit - QLineEdit object
+    return: bool
+        Is text property filled
+    """
     if line_edit.text() == "":
         line_edit.setProperty("wrongInput", True)
         redraw_widget(line_edit)
@@ -24,7 +32,12 @@ def is_filled(line_edit):
         return True
 
 
-def redraw_widget(widget):
+def redraw_widget(widget: QWidget) -> None:
+    """Helper for redraw stylesheet of any QWidget
+    args:
+        widget - QWidget object
+    return: None
+    """
     widget.style().unpolish(widget)
     widget.style().polish(widget)
     widget.update()
@@ -48,10 +61,10 @@ class LoginDialog(Ui_loginDialog, QDialog):
         self.shortcut = QShortcut(QKeySequence("Esc"), self)
         self.shortcut.activated.connect(self.cancel_form)
 
-    def cancel_form(self):
+    def cancel_form(self) -> None:
         self.close()
 
-    def accept_form(self):
+    def accept_form(self) -> None:
         if self.loginTabWidget.currentWidget().objectName() == "signInTab":
             if is_filled(self.loginLineEdit) and is_filled(self.passwordLineEdit):
                 self.action = "Login"
@@ -64,7 +77,16 @@ class LoginDialog(Ui_loginDialog, QDialog):
                 self.action = "Register"
                 self.close()
 
-    def return_result(self):
+    def return_result(self) -> namedtuple:
+        """Used to get input result after dialog closing
+        return:
+            Result: namedtuple
+                .action = ("Cancel", "Login", "Register")
+                .login: string login name
+                .password: string
+                .username: string Real name or display name
+                .email: string
+        """
         Result = namedtuple("Result", "action login password username email")
         if self.action == "Login":
             return Result(self.action, self.loginLineEdit.text(),

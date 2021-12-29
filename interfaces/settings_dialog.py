24a96dd382e474f4654552d478a5a6b2f1c61401
nekrod <NP17210@yandex.ru>
2021-12-29 10:47:40 +0500

    some changes)))

--- interfaces/settings_dialog.py
+++ interfaces/settings_dialog.py
@@ -6,13 +6,6 @@ from PySide6.QtWidgets import QDialog, QMainWindow, QWidget, QHBoxLayout, QLabel
 from interfaces.raw.setting_card import Ui_SettingCard
 from interfaces.raw.settings_dialog import Ui_SettingsDialog
 
-SETTINGS_LIST = [
-    {
-        "name": "Тема оформления",
-        "type": "combo_box"
-    }
-]
-
 
 class SettingItem(Ui_SettingCard, QWidget):
     def __init__(self):
@@ -22,15 +15,35 @@ class SettingItem(Ui_SettingCard, QWidget):
 
 
 class SettingsDialog(Ui_SettingsDialog, QDialog):
+    class SettingListItem:
+        def __init__(self,
+                     name: str,
+                     type_setting: str,
+                     method: object):
+            self.name = name
+            self.type = type_setting
+            self.method = method
+
+    SETTINGS_LIST = [
+        SettingListItem(
+            "Тема оформления",
+            "button",
+            "none"
+        )
+    ]
+
     def __init__(self, parent: QMainWindow):
         super(SettingsDialog, self).__init__()
 
         self.setupUi(self)
         self.setParent(parent, Qt.Window)
+        self.setWindowTitle("Settings")
 
         self.load_settings()
 
     def load_settings(self):
-        for setting in SETTINGS_LIST:
+        for setting in self.SETTINGS_LIST:
+            new_widget = SettingItem()
+            new_widget.label.setText(setting.name)
 
-            self.SettingsAreaLayout.addWidget(SettingItem())
+            self.SettingsAreaLayout.addWidget(new_widget)

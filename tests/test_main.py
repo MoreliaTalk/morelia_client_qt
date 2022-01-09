83c486435eeddce003a25582fe28f0c08749e393
nekrod <NP17210@yandex.ru>
2022-01-09 15:45:19 +0500

    fix

--- tests/test_main.py
+++ tests/test_main.py
@@ -11,17 +11,10 @@ class TestSetColorTheme(unittest.TestCase):
     def setUp(self):
         self.app = lib_for_tests.create_qapplication()
         self.mainWindow = main.MainWindow(self.app)
-    
-    def test_standart_color_theme(self):
-        self.assertEqual(self.mainWindow.set_color_theme(), self.mainWindow.styleSheet())
 
-    def test_custom_color_theme(self):
+    def test_color_theme(self):
         self.assertEqual(
-            self.mainWindow.set_color_theme(
-                primary_color="#{:06x}".format(random.randint(0, 0xFFFFFF)),
-                secondary_color="#{:06x}".format(random.randint(0, 0xFFFFFF)),
-                background_color="#{:06x}".format(random.randint(0, 0xFFFFFF))
-            ),
+            self.mainWindow.set_color_theme(),
             self.mainWindow.styleSheet()
         )
 

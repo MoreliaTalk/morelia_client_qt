5999d7a9e55a78c3f34e3e0f5633cbb003b43c8f
NekrodNIK <NP17210@yandex.ru>
2022-03-09 13:32:38 +0500

    merge develop

--- modules/server_connection/server_com.py
+++ modules/server_connection/server_com.py
@@ -3,13 +3,16 @@ from loguru import logger
 
 
 class ServerCom:
-    def __init__(self):
-        pass
+    def __init__(self, url: str):
+        self.url = url
+        self.connect()
 
-    async def connect_to_server(self, url: str):
-        self.ws = await websockets.client.connect(url)
+    async def connect(self):
+        self.ws = await websockets.client.connect(self.url)
         logger.info("Websocket connected to " + url)
 
-    async def disconnect_from_server(self):
+    async def disconnect(self):
         await self.ws.wait_closed()
         logger.info("Websocket connection closed")
+
+    async
